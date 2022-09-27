411a931028c892c095cd546c7da64072da37328e
ruffin hounsonnon <ruffinhounsounnon@gmail.com>
2022-09-27 03:29:30 +0100

    oui

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char *a, int size);
+void set_string(char **s, char *to);
